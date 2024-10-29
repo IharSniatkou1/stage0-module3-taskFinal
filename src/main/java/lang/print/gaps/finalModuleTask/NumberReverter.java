a054ee60ab7c18cd33c9c95cc9415e803bd96897
Ihar Sniatkou <iharsniatkou@gmail.com>
2024-10-29 19:25:47 +0100

    Stage0-module3-taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String reversed = new StringBuilder(String.valueOf(number)).reverse().toString();
+        System.out.println(reversed);
     }
 }
